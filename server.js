3271eae7cbb87d1cb2954ec361b07fcc3ae72a94
mrvs <mrvs@outlook.com>
2017-07-06 01:50:34 -0300

    server

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var app = express();
 var port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + '/app'));
+app.use('/bower_components', express.static(__dirname + '/bower_components'));
 app.listen(port, function() {
     console.log('App is running on port ' + port);
 });
