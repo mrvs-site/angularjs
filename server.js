a94e72eb4880d91afc3e51bdc40fb42e6a259f15
mrvs <mrvs@outlook.com>
2017-09-26 23:39:50 -0300

    server

--- server.js
+++ server.js
@@ -2,6 +2,19 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 8080;
 
+app.all('*', function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Credentials', false);
+    res.header('Access-Control-Max-Age', '86400');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+    next();
+});
+
+app.options('*', function(req, res) {
+    res.send(200);
+});
+
 app.use(express.static(__dirname + '/app'));
 app.use('/bower_components', express.static(__dirname + '/bower_components'));
 app.listen(port, function() {
