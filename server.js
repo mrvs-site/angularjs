98855f8b8339a7958ac67475e3cac72679747176
mrvs <mrvs@outlook.com>
2017-09-27 00:51:16 -0300

    s

--- server.js
+++ server.js
@@ -3,10 +3,13 @@ var app = express();
 var port = process.env.PORT || 8080;
 
 app.all('*', function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', req.headers.origin);
-    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Origin', 'https://viacep.com.br');
+    res.header('Access-Control-Allow-Methods', 'GET');
     res.header('Access-Control-Allow-Credentials', false);
-    res.header('Access-Control-Max-Age', '86400');
+    res.header('Access-Control-Max-Age', '1728000');
+    res.header('Content-type', 'application/json');
+    res.header('Cache-Control', 'no-cache');
+    res.header('Content-type', 'text/html; charset=utf-8');
     res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
     next();
 });
